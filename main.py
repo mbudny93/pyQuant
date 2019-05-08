20645453b60f7b1b8ec6cebdec8fcd9873356ddb
budny <marcin.budny@nokia.com>
2019-05-08 15:18:43 +0000

    up

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from updater import Updater
 
 import pymysql
 
-vendorName = 'quotemedia'
+connection = pymysql.connect(host='localhost', user='admin', password='admin')
+##############################################################################
 dbname = 'test'
 symbol_table_name = 'symbol'
 price_table_name = 'daily_price'
-connection = pymysql.connect(host='localhost', user='admin', password='admin')
+vendorName = 'quotemedia'
 ##############################################################################
 db = Database(connection, dbname)
 db.createTables(symbol_table_name, price_table_name)
 db.createTickersList()
+##############################################################################
 dbInserter = DataInserter(db)
 dbInserter.insertSymbols()
 ##############################################################################
 dataVendor = DataVendor(vendorName)
 updater = Updater(dbInserter, dataVendor)
 ##############################################################################
-##############################################################################
 updater.firstUpdate()
-##############################################################################
 ##############################################################################
